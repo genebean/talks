51bc46fab8fbf80549076a2fa5e0b3f1d36296ba
Gene Liverman <gene.liverman+5202103@puppet.com>
2017-07-28 23:51:25 -0700

    Initial site + ConfigMgmtCamp PDX talk

---
+++ 2017/08/03/index.md
@@ -0,0 +1,8 @@
+## Windows Configuration Management with Puppet
+
+* [Abstract](abstract)
+* [PDF](Windows-Configuration-Management-with-Puppet.pdf)
+* [PowerPoint](Windows-Configuration-Management-with-Puppet.pptx)
+* [Code sample](code_sample)
+
+Source code and other files from the presentation are under [src/](src/)
---
+++ 2017/08/03/src/win_patching.pp
@@ -0,0 +1,61 @@
+class { '::chocolatey':
+  log_output => true,
+}
+
+chocolateyfeature { 'autouninstaller':
+  ensure => enabled,
+}
+
+exec { 'Install NuGet package provider':
+  command   => '$(Install-PackageProvider -Name NuGet -Force)',
+  onlyif    => '$(if((Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue) -eq $null) { exit 0 } else { exit 1 })',
+  provider  => powershell,
+  logoutput => true,
+}
+
+exec { 'Install PSWindowsUpdate module':
+  command   => '$(Install-Module -Name PSWindowsUpdate -Scope AllUsers -Force)',
+  onlyif    => '$(if((Get-Module PSWindowsUpdate -list) -eq $null) { exit 0 } else { exit 1 })',
+  provider  => powershell,
+  logoutput => true,
+  require   => Exec['Install NuGet package provider'],
+}
+
+$maintenance_day  = lookup('windows::maintenance_day', { 'default_value' => 'Saturday'})
+
+schedule { 'Maintenance Window':
+  range   => '1:30 - 4:30',
+  weekday => $maintenance_day,
+}
+
+reboot { 'before':
+  when     => pending,
+  schedule => 'Maintenance Window',
+}
+
+package { 'powershell':
+  ensure   => latest,
+  provider => 'chocolatey',
+  schedule => 'Maintenance Window',
+}
+
+exec { 'Run Windows Update':
+  command   => '$(Get-WUInstall -MicrosoftUpdate -IgnoreUserInput -ShowSearchCriteria -AcceptAll -IgnoreReboot -Verbose)',
+  onlyif    => '$(if (@(Get-WUInstall -MicrosoftUpdate -IgnoreUserInput -ListOnly).Count -gt 0) { exit 0 } else { exit 1 })',
+  provider  => powershell,
+  logoutput => true,
+  schedule  => 'Maintenance Window',
+  require   => Exec['Install PSWindowsUpdate module'],
+}
+
+# Do not try and manage these same settings in a GPO as the two will conflict
+class { '::wsus_client' :
+  no_auto_update                      => true,
+  auto_update_option                  => 'NotifyOnly',
+  detection_frequency_hours           => 1,
+  no_auto_reboot_with_logged_on_users => false,
+  server_url                          => 'http://wsus.example.com',
+  #target_group                        => 'ServerUpdates',
+  purge_values                        => true,
+  before                              => Exec['Run Windows Update'],
+}
